a491d70971cc6a069c6da8a2b1ef0fd3d4aaef9e
Pdevu616 <pranith616@gmail.com>
2024-12-15 23:50:30 -0500

    NAme 1

--- script/UBToken.s.sol
+++ script/UBToken.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Script,console} from "forge-std/Script.sol";
 import {IERC20} from "../src/interfaces/IERC20.sol";
 
-contract totalSupply is Script {
+contract UBTokenDeploy is Script {
     
     address constant USDD=0x0C10bF8FcB7Bf5412187A595ab97a3609160b5c6;
 
